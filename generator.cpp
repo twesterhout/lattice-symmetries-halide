d1365a811a193d921ca3ea8b8f40e8cd45d8bbf4
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-08-10 14:35:24 +0200

    Initial commit

---
+++ generator.cpp
@@ -0,0 +1,203 @@
+#include "Halide.h"
+#include <cmath>
+#include <complex>
+
+// #include <cstdio>
+
+using namespace Halide;
+
+auto bit_permute_step_64(Expr x, Expr m, Expr d) -> Expr {
+  auto y = ((x >> d) ^ x) & m;
+  return (x ^ y) ^ (y << d);
+}
+
+class benes_network_generator
+    : public Halide::Generator<benes_network_generator> {
+public:
+  GeneratorParam<int> _spin_inversion{"spin_inversion", /*default value*/ 0};
+
+  Input<Buffer<uint64_t>> _x{"x", 1};
+  Input<uint64_t> _flip_mask{"flip_mask"};
+  Input<Buffer<uint64_t>> _masks{"masks", 2};
+  Input<Buffer<double>> _eigvals_re{"eigvals_re", 1};
+  Input<Buffer<double>> _eigvals_im{"eigvals_im", 1};
+  Input<Buffer<int>> _shifts{"shifts", 1};
+  Output<Buffer<uint64_t>> _repr{"representative", 1};
+  Output<Buffer<double>> _character{"character", 2};
+  Output<Buffer<double>> _norm{"norm"};
+
+  auto reduction_step_impl(Expr i, Tuple current, Expr y, Expr c_re,
+                           Expr c_im) const -> Tuple {
+    auto current_r = current[0];
+    auto current_c_re = current[1];
+    auto current_c_im = current[2];
+    auto current_n = current[3];
+
+    auto is_less = y < current_r;
+    auto next_r = select(is_less, y, current_r);
+    auto next_c_re = select(is_less, c_re, current_c_re);
+    auto next_c_im = select(is_less, c_im, current_c_im);
+
+    auto is_equal = y == _x(i);
+    auto next_n = select(is_equal, current_n + c_re, current_n);
+
+    return {next_r, next_c_re, next_c_im, next_n};
+  }
+
+  auto reduction_step(Expr i, Tuple current, Expr y, Expr c_re, Expr c_im) const
+      -> Tuple {
+    if (_spin_inversion == 0) {
+      return reduction_step_impl(i, current, y, c_re, c_im);
+    }
+    if (_spin_inversion == 1) {
+      auto temp = reduction_step_impl(i, current, y, c_re, c_im);
+      return reduction_step_impl(i, temp, y & _flip_mask, c_re, c_im);
+    }
+    if (_spin_inversion == -1) {
+      auto temp = reduction_step_impl(i, current, y, c_re, c_im);
+      return reduction_step_impl(i, temp, y & _flip_mask, -c_re, -c_im);
+    }
+    throw std::runtime_error{"invalid spin_inversion"};
+  }
+
+  auto reduction_step(Tuple current, Tuple other) const -> Tuple {
+    auto current_r = current[0];
+    auto current_c_re = current[1];
+    auto current_c_im = current[2];
+    auto current_n = current[3];
+    auto r = other[0];
+    auto c_re = other[1];
+    auto c_im = other[2];
+    auto n = other[3];
+
+    auto is_less = r < current_r;
+    auto next_r = select(is_less, r, current_r);
+    auto next_c_re = select(is_less, c_re, current_c_re);
+    auto next_c_im = select(is_less, c_im, current_c_im);
+    auto next_n = current_n + n;
+
+    return {next_r, next_c_re, next_c_im, next_n};
+  }
+
+  void generate() {
+    auto depth = _masks.dim(0).extent();
+    auto number_masks = _masks.dim(1).extent();
+    auto const chunk_size = natural_vector_size(type_of<uint64_t>());
+    auto number_chunks = number_masks / chunk_size;
+    auto number_rest = number_masks - number_chunks * chunk_size;
+
+    Var i{"i"};
+
+    // Apply masks to generate transformed spin configurations
+    RDom k{0, depth, "k"};
+    Func y_batched{"y_batched"};
+    Var j_outer{"j_outer"}, j_inner{"j_inner"};
+    y_batched(i, j_outer, j_inner) = _x(i);
+    y_batched(i, j_outer, j_inner) = bit_permute_step_64(
+        y_batched(i, j_outer, j_inner),
+        _masks(k, j_outer * chunk_size + j_inner), _shifts(k));
+
+    Func y_scalar{"y_scalar"};
+    Var j_tail{"j_tail"};
+    y_scalar(i, j_tail) = _x(i);
+    y_scalar(i, j_tail) =
+        bit_permute_step_64(y_scalar(i, j_tail), _masks(k, j_tail), _shifts(k));
+
+    // Compute vectorized reduction
+    RDom m_main{0, number_chunks, "m_main"};
+    Func custom_reduction{"custom_reduction"};
+    custom_reduction(i, j_inner) =
+        Tuple{_x(i), cast<double>(1), cast<double>(0), cast<double>(0)};
+    custom_reduction(i, j_inner) = reduction_step(
+        i, custom_reduction(i, j_inner), y_batched(i, m_main, j_inner),
+        _eigvals_re(m_main * chunk_size + j_inner),
+        _eigvals_im(m_main * chunk_size + j_inner));
+
+    // Reduce across one chunk
+    RDom m_lane{1, chunk_size - 1, "m_lane"};
+    Func reduction_lane{"reduction_lane"};
+    reduction_lane(i) =
+        Tuple{custom_reduction(i, 0)[0], custom_reduction(i, 0)[1],
+              custom_reduction(i, 0)[2], custom_reduction(i, 0)[3]};
+    reduction_lane(i) =
+        reduction_step(reduction_lane(i), custom_reduction(i, m_lane));
+
+    // Compute reduction over remaining elements
+    Func reduction_scalar{"reduction_scalar"};
+    RDom m_tail{number_chunks * chunk_size, number_rest, "m_tail"};
+    reduction_scalar(i) = Tuple{reduction_lane(i)[0], reduction_lane(i)[1],
+                                reduction_lane(i)[2], reduction_lane(i)[3]};
+    reduction_scalar(i) =
+        reduction_step(i, reduction_scalar(i), y_scalar(i, m_tail),
+                       _eigvals_re(m_tail), _eigvals_im(m_tail));
+
+    // Store results
+    Var q;
+    _repr(i) = reduction_scalar(i)[0];
+    _character(i, q) = undef<double>();
+    _character(i, 0) = reduction_scalar(i)[1];
+    _character(i, 1) = reduction_scalar(i)[2];
+    _norm(i) = reduction_scalar(i)[3];
+
+    auto batch_size = _x.dim(0).extent();
+    _x.dim(0).set_min(0).set_stride(1);
+    _masks.dim(0).set_min(0).set_stride(number_masks);
+    _masks.dim(1).set_min(0).set_stride(1);
+    _eigvals_re.dim(0).set_min(0).set_stride(1).set_extent(number_masks);
+    _eigvals_im.dim(0).set_min(0).set_stride(1).set_extent(number_masks);
+    _shifts.dim(0).set_min(0).set_stride(1).set_extent(depth);
+    _repr.dim(0).set_min(0).set_stride(1).set_extent(batch_size);
+    _character.dim(0).set_min(0).set_stride(2).set_extent(batch_size);
+    _character.dim(1).set_min(0).set_stride(1).set_extent(2);
+    _norm.dim(0).set_min(0).set_stride(1).set_extent(batch_size);
+
+    // Schedule
+    y_batched.compute_at(custom_reduction, m_main);
+
+    custom_reduction.compute_at(reduction_scalar, i);
+    custom_reduction.vectorize(j_inner);
+    custom_reduction.update(0).vectorize(j_inner);
+    // reduction_lane.compute_at(reduction_scalar, i);
+    reduction_scalar.compute_at(_repr, i);
+    _character.update(0).compute_with(_repr, i);
+    _character.update(1).compute_with(_repr, i);
+    _norm.compute_with(_repr, i);
+  }
+};
+
+#if 0
+class experimental_generator
+    : public Halide::Generator<experimental_generator> {
+public:
+  Input<Buffer<double>> _input{"input", 1};
+  Output<double> _output_1{"output_1"};
+  Output<double> _output_2{"output_2"};
+
+  void generate() {
+    Var i{"i"};
+
+    Func temp{"temp"};
+    temp(i) = sin(_input(i));
+
+    RDom j_reduction{0, _input.dim(0).extent(), "j_reduction"};
+
+    Func reduction{"reduction"};
+    reduction() = {cast<double>(0), cast<double>(INFINITY)};
+    reduction() = {reduction()[0] + temp(j_reduction),
+                   min(reduction()[1], temp(j_reduction))};
+    reduction.compute_root();
+    temp.compute_at(reduction, j_reduction);
+
+    _output_1() = reduction()[0];
+    _output_2() = reduction()[1];
+
+    // temp.compute_at(_output_1, j_reduction);
+    // _output_2.compute_with(_output_1, j_reduction);
+  }
+};
+#endif
+
+HALIDE_REGISTER_GENERATOR(benes_network_generator, benes_network_generator)
+#if 0
+HALIDE_REGISTER_GENERATOR(experimental_generator, experimental_generator)
+#endif
---
+++ test/run_tests.cpp
@@ -0,0 +1,188 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "benes_network.h"
+#include <HalideBuffer.h>
+#include <complex>
+#include <doctest.h>
+#include <memory>
+
+using namespace Halide::Runtime;
+
+#if 0
+TEST_CASE("benes_network") {
+
+  uint64_t x = 0b0001000100;
+  Buffer<uint64_t> masks(7);
+  masks(0) = 0;
+  masks(1) = 0;
+  masks(2) = 0;
+  masks(3) = 1;
+  masks(4) = 1;
+  masks(5) = 17;
+  masks(6) = 341;
+  Buffer<int> shifts(7);
+  shifts(0) = 1;
+  shifts(1) = 2;
+  shifts(2) = 4;
+  shifts(3) = 8;
+  shifts(4) = 4;
+  shifts(5) = 2;
+  shifts(6) = 1;
+
+  uint64_t r;
+  auto r_buffer = Buffer<uint64_t>::make_scalar(&r);
+
+  benes_network(x, masks, shifts, r_buffer);
+  printf("%lu\n", r_buffer.number_of_elements());
+  printf("%lu, %lu\n", x, r);
+}
+#endif
+
+#if 1
+TEST_CASE("4-spin chain (no symmetries)") {
+  // 4 networks of depth 3
+  uint64_t masks = 0;
+  double eigvals = 0;
+  int shifts[3] = {1, 2, 1};
+  Buffer<uint64_t> masks_buffer{&masks, 0, 3};
+  masks_buffer.transpose(0, 1);
+  Buffer<double> eigvals_re_buffer{&eigvals, 0};
+  Buffer<double> eigvals_im_buffer{&eigvals, 0};
+  Buffer<int> shifts_buffer{&shifts[0], 3};
+
+  uint64_t repr[1];
+  std::complex<double> character[1];
+  double norm[1];
+
+  for (auto i = 0; i < 1; ++i) {
+    repr[i] = std::numeric_limits<uint64_t>::max();
+  }
+
+  Buffer<uint64_t> repr_buffer{&repr[0], 1};
+  Buffer<double> character_buffer{reinterpret_cast<double *>(&character), 2, 1};
+  character_buffer.transpose(0, 1);
+  Buffer<double> norm_buffer{&norm[0], 1};
+
+  uint64_t x[1] = {0b0110};
+  Buffer<uint64_t> x_buffer{&x[0], 1};
+  benes_network(x_buffer, uint64_t{0b1111}, masks_buffer, eigvals_re_buffer,
+                eigvals_im_buffer, shifts_buffer, repr_buffer, character_buffer,
+                norm_buffer);
+
+  for (auto i = 0; i < 1; ++i) {
+    fprintf(stderr, "%lu -> %lu, %f + %fi, %f\n", x[i], repr[i],
+            character[i].real(), character[i].imag(), norm[i]);
+  }
+}
+#endif
+
+#if 0
+TEST_CASE("3-spin chain (no symmetries)") {
+  // 4 networks of depth 3
+  uint64_t masks[3][1] = {{0}, {0}, {0}};
+  double eigvals_re[1] = {1};
+  double eigvals_im[1] = {0};
+  int shifts[3] = {1, 2, 1};
+
+  Buffer<uint64_t> masks_buffer{&masks[0][0], 0, 3};
+  masks_buffer.transpose(0, 1);
+  Buffer<double> eigvals_re_buffer{&eigvals_re[0], 0};
+  Buffer<double> eigvals_im_buffer{&eigvals_im[0], 0};
+  Buffer<int> shifts_buffer{&shifts[0], 3};
+
+  uint64_t repr[1];
+  std::complex<double> character[1];
+  double norm[1];
+
+  Buffer<uint64_t> repr_buffer{&repr[0], 1};
+  Buffer<double> character_buffer{reinterpret_cast<double *>(&character), 2, 1};
+  character_buffer.transpose(0, 1);
+  Buffer<double> norm_buffer{&norm[0], 1};
+
+  uint64_t x[1] = {0b010};
+  Buffer<uint64_t> x_buffer{&x[0], 1};
+  benes_network(x_buffer, uint64_t{0b111}, masks_buffer, eigvals_re_buffer,
+                eigvals_im_buffer, shifts_buffer, repr_buffer, character_buffer,
+                norm_buffer);
+
+  {
+    auto const i = 0;
+    printf("%lu -> %lu, %f + %fi, %f\n", x[i], repr[i], character[i].real(),
+           character[i].imag(), norm[i]);
+  }
+}
+#endif
+
+#if 0
+TEST_CASE("3-spin chain (translation only)") {
+  // 4 networks of depth 3
+  uint64_t masks[3][3] = {{0, 4, 0}, {1, 2, 0}, {1, 5, 0}};
+  double eigvals_re[4] = {1, 1, 1};
+  double eigvals_im[4] = {0, 0, 0};
+  int shifts[3] = {1, 2, 1};
+
+  Buffer<uint64_t> masks_buffer{&masks[0][0], 3, 3};
+  masks_buffer.transpose(0, 1);
+  Buffer<double> eigvals_re_buffer{&eigvals_re[0], 3};
+  Buffer<double> eigvals_im_buffer{&eigvals_im[0], 3};
+  Buffer<int> shifts_buffer{&shifts[0], 3};
+
+  uint64_t repr[8];
+  std::complex<double> character[8];
+  double norm[8];
+
+  Buffer<uint64_t> repr_buffer{&repr[0], 8};
+  Buffer<double> character_buffer{reinterpret_cast<double *>(&character), 2, 8};
+  character_buffer.transpose(0, 1);
+  Buffer<double> norm_buffer{&norm[0], 8};
+
+  uint64_t x[16] = {0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111};
+  Buffer<uint64_t> x_buffer{&x[0], 8};
+  benes_network(x_buffer, uint64_t{0b111}, masks_buffer, eigvals_re_buffer,
+                eigvals_im_buffer, shifts_buffer, repr_buffer, character_buffer,
+                norm_buffer);
+
+  for (auto i = 0; i < 8; ++i) {
+    printf("%lu -> %lu, %f + %fi, %f\n", x[i], repr[i], character[i].real(),
+           character[i].imag(), norm[i]);
+  }
+}
+#endif
+
+#if 0
+TEST_CASE("4-spin chain (translation only)") {
+  // 4 networks of depth 3
+  uint64_t masks[3][4] = {{0, 0, 0, 0}, {1, 3, 2, 0}, {5, 0, 5, 0}};
+  double eigvals_re[4] = {1, 1, 1, 1};
+  double eigvals_im[4] = {0, 0, 0, 0};
+  int shifts[3] = {1, 2, 1};
+
+  Buffer<uint64_t> masks_buffer{&masks[0][0], 4, 3};
+  masks_buffer.transpose(0, 1);
+  Buffer<double> eigvals_re_buffer{&eigvals_re[0], 4};
+  Buffer<double> eigvals_im_buffer{&eigvals_im[0], 4};
+  Buffer<int> shifts_buffer{&shifts[0], 3};
+
+  uint64_t repr[16];
+  std::complex<double> character[16];
+  double norm[16];
+
+  Buffer<uint64_t> repr_buffer{&repr[0], 16};
+  Buffer<double> character_buffer{reinterpret_cast<double *>(&character), 2,
+                                  16};
+  character_buffer.transpose(0, 1);
+  Buffer<double> norm_buffer{&norm[0], 16};
+
+  uint64_t x[16] = {0b0000, 0b0001, 0b0010, 0b0011, 0b0100, 0b0101,
+                    0b0110, 0b0111, 0b1000, 0b1001, 0b1010, 0b1011,
+                    0b1100, 0b1101, 0b1110, 0b1111};
+  Buffer<uint64_t> x_buffer{&x[0], 16};
+  benes_network(x_buffer, uint64_t{0b1111}, masks_buffer, eigvals_re_buffer,
+                eigvals_im_buffer, shifts_buffer, repr_buffer, character_buffer,
+                norm_buffer);
+
+  for (auto i = 0; i < 16; ++i) {
+    printf("%lu -> %lu, %f + %fi, %f\n", x[i], repr[i], character[i].real(),
+           character[i].imag(), norm[i]);
+  }
+}
+#endif
